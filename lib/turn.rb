66a94cef35afd4cdaaa9ae6c0d9d95a8f4323aac
Ethan <ethansagin@gmail.com>
2019-02-26 20:31:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,8 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    new_board = move(board, index)
+    new_board
   else
     turn(board)
   end
