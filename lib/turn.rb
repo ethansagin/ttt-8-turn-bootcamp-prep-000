695ca1b1a633b980ad8964660340751689872c30
Ethan <ethansagin@gmail.com>
2019-02-26 19:57:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,3 +7,20 @@ def display_board(x)
   puts "-----------"
   puts " #{x[6]} | #{x[7]} | #{x[8]} "
 end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index)
+    false 
+  elsif index.between?(0, 8)
+    true
+  end
+end
+
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
